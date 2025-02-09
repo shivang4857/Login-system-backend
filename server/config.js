973f7d54859a9ffe4e2896495e0d04e561b49f47
shivang4857 <shivangd801@gmail.com>
2025-02-09 14:37:26 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "hello",
+    EMAIL: "steve.franecki@ethereal.email", // testing email & password
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI: "mongodb+srv://shivang_4857:yQeCKxnqJH2GWIdG@cluster0.yqamvwn.mongodb.net/"
+}
